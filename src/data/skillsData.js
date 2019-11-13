028b6e7a91338b00c6d7000890c5e8a2682be036
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-11-13 10:47:25 -0500

    Update skillsData.js

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,27 +1,27 @@
 export const skillsData = [
     {
         category: 'Languages',
-        skills: 'JavaScript, HTML, CSS, Python',
+        skills: 'JavaScript, HTML3, CSS3, Python',
     },
     {
         category: 'Libraries | Frameworks',
         skills: 'React, Redux, Vue',
     },
     {
-        category: 'Back-End (some experience)',
-        skills: 'Node.js, Express',
-    },
-    {
         category: 'Deployment',
         skills: 'Netlify, AWS (Lambda, S3), GoDaddy',
     },
     {
-        category: 'Databases',
+        category: 'Back-End (novice)',
+        skills: 'Node.js, Express',
+    },
+    {
+        category: 'Databases (novice)',
         skills: 'MongoDB, MySQL',
     },
     {
         category: 'Concepts',
-        skills: 'Responsive Design, Wireframing, Web Accessibility, OOP, Agile Development, Mobile-First Development',
+        skills: 'Responsive Design, Web Accessibility, Wireframing, OOP, Mobile-First Development, Agile Development',
     },
     {
         category: 'Tools',
