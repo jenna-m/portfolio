74152d311fc037e45c7945d4deb34def2fc5e472
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-11-20 18:07:47 -0500

    Update workData.js

--- src/data/workData.js
+++ src/data/workData.js
@@ -47,7 +47,7 @@ export const workData = [
         category: 'web',
         displayCategory: 'Web Development & Design',
         tools: 'React, Formik, emailJS, react-stack-grid',
-        description: 'Artist and instructor Norman Ed was in need of a website redesign. On top of the refresh, pages to showcase his past/present exhibitions and mentions in the press, a biography, contact form, and a randomized custom 404 page were added.',
+        description: 'Artist and instructor Norman Ed was in need of a website redesign. On top of the redesign, pages to showcase his past/present exhibitions and mentions in the press, a biography, contact form, and a randomized custom 404 page were added.',
         github: 'https://github.com/jenna-m/norman-ed',
         websiteURL: 'https://normaned.com',
         src: [
@@ -62,7 +62,7 @@ export const workData = [
         category: 'web',
         displayCategory: 'Web Development & Design',
         tools: 'React, Redux, Lodash',
-        description: 'A personal project, this online storefront for a fictional store gave me experience with Redux. Included is a randomized call to action on the home page, the ability to display items by category, a shopping cart, image magnification, and a randomized custom 404 page. Images from Madewell.',
+        description: 'A personal project, this fictitious online storefront gave me experience with Redux. Included is a randomized call to action on the home page, the ability to display items by category, a shopping cart, image magnification, and a randomized custom 404 page. Images from Madewell.',
         github: 'https://github.com/jenna-m/redux-co',
         websiteURL: 'https://redux-co.netlify.com',
         src: [
@@ -78,7 +78,7 @@ export const workData = [
         category: 'graphic',
         displayCategory: 'Graphic Design',
         tools: 'Adobe Photoshop, Illustrator',
-        description: 'Adorn was in need of a logo redesign and storefront window art. For the logo redesign, I incorporated a softer blush color to contrast the modern geometric design. For the window art, I stayed true to the minimal and geometric direction.',
+        description: 'Jewelry and home decor storefront Adorn needed a logo redesign and window art. For the logo redesign, I incorporated a softer blush color to contrast the modern geometric design. For the window art, I stayed true to the minimal and geometric direction.',
         src: [
             adornLogo,
             adornWindow1,
@@ -92,7 +92,7 @@ export const workData = [
         category: 'graphic',
         displayCategory: 'Graphic Design',
         tools: 'Adobe Illustrator',
-        description: 'YouTube channel Baking Demystified needed a logo and channel art. I aimed to keep the look playful with the font selection and bright colors.',
+        description: 'The informational YouTube channel Baking Demystified was in need of logo creation and channel art. I aimed to keep the look playful with the font selection and bright color.',
         src: [
             bakingDemystifiedYoutube,
             bakingDemystifiedLogo1,
@@ -107,7 +107,7 @@ export const workData = [
         category: 'graphic',
         displayCategory: 'Graphic Design',
         tools: 'Adobe Photoshop',
-        description: 'The artist needed a demo cover design for a musical digital release in 2014. The direction was black & white photography of discarded technology, with a geometric twist. I also created social media graphics for the artist, featuring photographs of the artist in nature.',
+        description: 'The artist needed a demo cover design for a digital music release in 2014. The direction was black & white photography of discarded technology, with a geometric twist. I also created social media graphics for the artist, featuring photographs of the artist in nature.',
         src: [
             floorOverheadDemo1,
             floorOverheadSocial1,
@@ -127,7 +127,7 @@ export const workData = [
         category: 'graphic',
         displayCategory: 'Graphic Design',
         tools: 'Adobe Photoshop, Illustrator, & Muse',
-        description: 'Indoor Cities needed an album cover design for a musical digital release in 2013. The direction was to use natural photography and geometry. Their website was kept minimal to direct users to social media outlets and to showcase their music with select reviews and use of the Bandcamp plugin. I also created social media graphics, gig posters, and business cards for the group.',
+        description: 'Indoor Cities needed an album cover design for a digital music release in 2013. The direction was to use natural photography and geometry. Their website was kept minimal to direct users to social media outlets and to showcase their music with select reviews and use of the Bandcamp plugin. I also created social media graphics, gig posters, and business cards for the band.',
         src: [
             indoorCitiesAlbum,
             indoorCitiesWebsite,
