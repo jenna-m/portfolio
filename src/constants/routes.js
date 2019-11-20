85e6cee4b9b4b5d585850dd988ea8b914bfeb6d9
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-11-20 09:56:58 -0500

    Remove About page

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,7 +5,6 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../components/home';
 import Work from '../components/pages/work/work';
 import ProjectPage from '../components/pages/work/projectPage';
-import About from '../components/pages/about/about';
 import Contact from '../components/pages/contact/contact';
 import NotFound from '../components/404';
 
@@ -15,7 +14,6 @@ export const Routes =
             <Route exact path="/" component={ Home } />
             <Route path ="/work" component={ Work } />
             <Route path="/project" component={ ProjectPage } />
-            <Route path="/about" component={ About } />
             <Route path="/contact" component={ Contact } />
             <Route component={ NotFound } />
         </Switch>
