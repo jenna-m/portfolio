0b0f7ca519678bd6af4dad116573d07b5738433b
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-11-20 18:07:17 -0500

    Update 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundImg from '../assets/graphics/404.svg';
 
 export default function NotFound() {
     return (
         <div className="_404-content">
-            <div>
-                <h1>404 / Not Found</h1>
+            <div id="_404-image">
+                <img src={ notFoundImg } alt="404" />
             </div>
-            <div>
-                <h2>¯\_(ツ)_/¯</h2>
+            <div id="_404-text">
+                <h2>Sorry...</h2>
+                <p>An error has occured, and the requested page was not found.</p>
+                <Link to="/">
+                    <button>Let's go home</button></Link>
             </div>
         </div>
     );
