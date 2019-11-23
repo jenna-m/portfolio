2bdf4212c2edc9cb4fa229d4a0c1480c685ea54b
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-11-23 14:43:51 -0500

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// I had to hard code these, but I got it to work now! ¯\_(ツ)_/¯
-
-export const env = {
-  REACT_APP_EMAILJS_RECEIVER: 'thesemdrnsocks@gmail.com',
-  REACT_APP_EMAILJS_TEMPLATEID: 'normanedcontactform',
-  REACT_APP_EMAILJS_USERID: 'user_cVacpT4HoGWc7Omos0X8P'
-};
