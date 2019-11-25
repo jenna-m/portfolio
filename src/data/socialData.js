89a74a94799962fbd81eb09d321e79f592424a6d
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-11-25 15:53:56 -0500

    Update socialData.js
    Add tooltips to social links

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -15,7 +15,9 @@ export default function Social() {
                         href="https://github.com/jenna-m" 
                         target="_blank"
                         rel="noopener noreferrer"
-                        id="github-link">
+                        id="github-link"
+                        data-tooltip="GitHub"
+                        data-tooltip-location="bottom">
                             { githubIcon }
                     </a>
                 </li>
@@ -24,17 +26,10 @@ export default function Social() {
                         href="https://angel.co/jennamichaels" 
                         target="_blank"
                         rel="noopener noreferrer"
-                        id="angel-link">
+                        id="angel-link"
+                        data-tooltip="AngelList"
+                        data-tooltip-location="bottom">
                             { angelListIcon }
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.canva.com/design/DADsDSMzEHc/iVNMgHehzb4ziKE0TOPzOA/view?website#4" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="resume-link">
-                            { resumeIcon }
                     </a>
                 </li>
                 <li>
@@ -42,8 +37,21 @@ export default function Social() {
                         href="https://www.linkedin.com/in/jenna-michaels/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        id="linkedin-link">
+                        id="linkedin-link"
+                        data-tooltip="LinkedIn"
+                        data-tooltip-location="bottom">
                             { linkedinIcon }
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.canva.com/design/DADsDSMzEHc/iVNMgHehzb4ziKE0TOPzOA/view?website#4" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        id="resume-link"
+                        data-tooltip="Resume"
+                        data-tooltip-location="bottom">
+                            { resumeIcon }
                     </a>
                 </li>
             </ul>
