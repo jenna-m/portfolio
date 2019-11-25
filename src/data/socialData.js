56a398b200aeec05bbf2f87ff8474fccde1a22d1
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-11-25 18:24:08 -0500

    Update socialData.js
    Reconfigure social links

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -9,64 +9,62 @@ import {
 
 export default function Social() {
     return (
-        <div>
-            <ul id="social-links">
-                <li>
-                    <a
-                        href="https://angel.co/jennamichaels" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="angel-link"
-                        data-tooltip="AngelList"
-                        data-tooltip-location="bottom">
-                            { angelListIcon }
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.linkedin.com/in/jenna-michaels/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="linkedin-link"
-                        data-tooltip="LinkedIn"
-                        data-tooltip-location="bottom">
-                            { linkedinIcon }
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.canva.com/design/DADsDSMzEHc/iVNMgHehzb4ziKE0TOPzOA/view?website#4" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="resume-link"
-                        data-tooltip="Resume"
-                        data-tooltip-location="bottom">
-                            { resumeIcon }
-                    </a>
-                </li>
-                <li>
-                    <a 
-                        href="https://github.com/jenna-m" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="github-link"
-                        data-tooltip="GitHub"
-                        data-tooltip-location="bottom">
-                            { githubIcon }
-                    </a>
-                </li>
-                <li>
-                    <a 
-                        href="https://stackoverflow.com/story/jennamichaels" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        id="stackoverflow-link"
-                        data-tooltip="SO"
-                        data-tooltip-location="bottom">
-                            { stackOverflowIcon }
-                    </a>
-                </li>
-            </ul>
+        <div id="social-link-container">
+            <div className="social-link">
+                <a
+                href="https://angel.co/jennamichaels" 
+                target="_blank"
+                rel="noopener noreferrer"
+                id="angel-link"
+                data-tooltip="AngelList"
+                data-tooltip-location="bottom">
+                    { angelListIcon }
+                </a>
+            </div>
+            <div className="social-link">
+                <a
+                href="https://www.linkedin.com/in/jenna-michaels/"
+                target="_blank"
+                rel="noopener noreferrer"
+                id="linkedin-link"
+                data-tooltip="LinkedIn"
+                data-tooltip-location="bottom">
+                    { linkedinIcon }
+                </a>
+            </div>
+            <div className="social-link">
+                <a
+                href="https://www.canva.com/design/DADsDSMzEHc/iVNMgHehzb4ziKE0TOPzOA/view?website#4" 
+                target="_blank"
+                rel="noopener noreferrer"
+                id="resume-link"
+                data-tooltip="Resume"
+                data-tooltip-location="bottom">
+                    { resumeIcon }
+                </a>
+            </div>
+            <div className="social-link">
+                <a 
+                href="https://github.com/jenna-m" 
+                target="_blank"
+                rel="noopener noreferrer"
+                id="github-link"
+                data-tooltip="GitHub"
+                data-tooltip-location="bottom">
+                    { githubIcon }
+                </a>
+            </div>
+            <div className="social-link">
+                <a 
+                href="https://stackoverflow.com/story/jennamichaels" 
+                target="_blank"
+                rel="noopener noreferrer"
+                id="stackoverflow-link"
+                data-tooltip="SO"
+                data-tooltip-location="bottom">
+                    { stackOverflowIcon }
+                </a>
+            </div>
         </div>
     )
 }
