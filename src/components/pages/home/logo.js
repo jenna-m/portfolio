7d10ea287c0e37802ca45d8b3ca9373f68ab3593
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-11-28 00:55:27 -0500

    Update graphics

---
+++ src/components/pages/home/logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../../assets/graphics/logoLG.svg';
+
+export default function Logo() {
+	return (
+		<div id="logo-wrapper">
+			<img src={logo} alt="Jenna Michaels Logo" />
+		</div>
+	);
+}
