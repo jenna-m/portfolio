0da1ddfb595bcc6342c77680534e84c4b7d45023
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-12-05 15:40:06 -0500

    Update skillsData.js
    Change IDs to classes

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -4,45 +4,47 @@ export default function Skills() {
 	return (
 		<div id="skills-container">
 			<div className="skills-card">
-				<p id="skill-category">Languages</p>
-				<p id="skill-list">JavaScript ES6+, HTML3, CSS3, Python3</p>
+				<p className="skill-category">Languages</p>
+				<p className="skill-list">
+					JavaScript ES6+, HTML3, CSS3, Python3
+				</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Libraries | Frameworks</p>
-				<p id="skill-list">React, Redux</p>
+				<p className="skill-category">Libraries | Frameworks</p>
+				<p className="skill-list">React, Redux</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Deployment</p>
-				<p id="skill-list">Netlify, AWS (Lambda, S3)</p>
+				<p className="skill-category">Deployment</p>
+				<p className="skill-list">Netlify, AWS (Lambda, S3)</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Back-End (novice)</p>
-				<p id="skill-list">Node.js, Express</p>
+				<p className="skill-category">Back-End (novice)</p>
+				<p className="skill-list">Node.js, Express</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Databases (novice)</p>
-				<p id="skill-list">MongoDB, MySQL</p>
+				<p className="skill-category">Databases (novice)</p>
+				<p className="skill-list">MongoDB, MySQL</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Tools</p>
-				<p id="skill-list">VSCode, GitHub, npm</p>
+				<p className="skill-category">Tools</p>
+				<p className="skill-list">VSCode, GitHub, npm</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Applications</p>
-				<p id="skill-list">
+				<p className="skill-category">Applications</p>
+				<p className="skill-list">
 					Adobe Photoshop, Illustrator, and XD; Autodesk Fusion 360
 				</p>
 			</div>
 
 			<div className="skills-card">
-				<p id="skill-category">Concepts</p>
-				<p id="skill-list">
+				<p className="skill-category">Concepts</p>
+				<p className="skill-list">
 					Web Accessibility, Wireframing, OOP, Responsive Design,
 					Mobile-First Development, Git Version Control, Agile
 					Methodology
