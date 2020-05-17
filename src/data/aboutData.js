50b02aead7109a7f625cfee499b533955b00c1de
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-05-17 14:04:48 -0400

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -5,16 +5,16 @@ export default function About() {
 	return (
 		<div id="about-section">
 			<div id="about-image">
-				<img src={profile} alt="Portrait of Jenna Michaels" />
+				<img src={profile} alt="Jenna Michaels sitting at her desk" />
 			</div>
 			<div id="about-text">
 				<p>
 					Jenna resides in Johnstown, Pennsylvania, where she works
 					from home as a freelance Graphic Designer and Front-End Web
 					Developer. She enjoys working with React, HTML, and CSS.
-					With a strong background in graphic arts, she strives to
-					fuse her skills to create functional, accessible, beautiful
-					web applications.
+					With a strong background in graphic arts, she combines her
+					skills to create interesting, accessible, beautiful web
+					applications.
 				</p>
 
 				<p>
